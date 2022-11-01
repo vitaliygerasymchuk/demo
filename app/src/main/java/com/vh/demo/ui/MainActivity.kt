60848e031ce5841451a006c84bb81aed96a423df
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2022-11-01 08:31:21 +0200

    setup abs

---
+++ app/src/main/java/com/vh/demo/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.vh.demo.ui
+
+import com.vh.demo.databinding.ActivityMainBinding
+import com.vh.demo.ui.abs.AbsBindingActivity
+
+class MainActivity : AbsBindingActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun onInitViews() {
+
+    }
+}
